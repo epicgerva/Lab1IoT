5a2bac137cfb86e0d6dd41e5784dacf9cb4cc077
Joaquín Cuitiño <3134745+Vextil@users.noreply.github.com>
2025-04-30 23:58:08 -0300

    Librerías de delay y led

--- sample_project/main/main.c
+++ sample_project/main/main.c
@@ -1,29 +1,22 @@
 #include <stdio.h>
-#include "led_strip.h"
-
+#include "led.h"
+#include "delay.h"
 
 void app_main(void)
 {
-    led_strip_t *strip = NULL;
-
-    // Inicializa el LED (esto configura el RMT, asigna memoria, etc.)
-    if (led_rgb_init(&strip) != ESP_OK) {
-        printf("Error al inicializar la tira de LED\n");
-        return;
+    while (1)
+    {
+        set_led(255, 0, 0);
+        delay_s(1);
+        set_led(0, 255, 0);
+        delay_s(1);
+        set_led(0, 0, 255);
+        delay_s(1);
+        set_led(255, 0, 0);
+        delay_ms(200);
+        set_led(0, 255, 0);
+        delay_ms(200);
+        set_led(0, 0, 255);
+        delay_ms(200);
     }
-
-    // Enciende el primer LED con color rojo (R=255, G=0, B=0)
-    strip->set_pixel(strip, 0, 255, 0, 0);
-
-    // Refresca para enviar los datos al LED
-    strip->refresh(strip, 100);
-
-    // // Espera 5 segundos
-    // vTaskDelay(pdMS_TO_TICKS(5000));
-
-    // // Apaga el LED
-    // strip->clear(strip, 100);
-
-    // // Libera recursos
-    // strip->del(strip);
 }
