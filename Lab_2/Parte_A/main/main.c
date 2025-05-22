6292579cb6f01b9fc220adf50877c43e16e77ddf
Joaquín Cuitiño <3134745+Vextil@users.noreply.github.com>
2025-05-22 16:09:31 -0300

    Agrego modulos de touch y board

--- Lab_2/Parte_A/main/main.c
+++ Lab_2/Parte_A/main/main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include "led.h"
 #include "delay.h"
+#include "touch.h"
+#include "board.h"
 
 void app_main(void)
 {
+    touch_init();
     while (1)
     {
         set_led(255, 0, 0);
