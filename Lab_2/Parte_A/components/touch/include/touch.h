0befdb29f3d0c871f0de9453e7c859a8be440f7d
Joaquín Cuitiño <3134745+Vextil@users.noreply.github.com>
2025-05-28 17:58:30 -0300

    Touch funcionando

--- Lab_2/Parte_A/components/touch/include/touch.h
+++ Lab_2/Parte_A/components/touch/include/touch.h
@@ -1,59 +1,20 @@
-// Copyright 2019 Espressif Systems (Shanghai) PTE LTD
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-#pragma once
+#ifndef TOUCH_H
+#define TOUCH_H
 
-#ifdef __cplusplus
-extern "C" {
+#include <stdbool.h>
+
+#define TOUCH_VOLUME_UP 1
+#define TOUCH_PLAY_PAUSE 2
+#define TOUCH_RING 4
+#define TOUCH_RECORD 5
+#define TOUCH_PHOTO 6
+#define TOUCH_NETWORK 11
+#define TOUCH_VOLUME_DOWN 14
+
+void touch_init(void);
+
+void touch_update(void);
+
+bool touch_pressed(uint8_t button);
+
 #endif
-
-/**
- * @brief Get touch status
- *
- * @param status get status
- *             
- */
-void touch_get_status(uint32_t *status);
-
-/**
- * @brief Get touch num
- *
- * @param status get num
- *             
- */
-void touch_get_num(uint32_t *num);
-
-/**
- * @brief Get touch mask (Each state of each pad corresponds to a mask) 
- *
- * @param status get mask          
- */
-void touch_get_mask(uint32_t *mask);
-
-/**
- * @brief Get touch status flag if it's triggered
- *
- * @param flag True : be actived    
- *             flase: be inactived   
- */
-void touch_get_flag_status(bool *flag);
-
-/**
- * @brief Initialize the touch        
- */
-void touch_init();
-
-#ifdef __cplusplus
-}
-#endif
-
--- Lab_2/Parte_A/main/main.c
+++ Lab_2/Parte_A/main/main.c
@@ -1,25 +1,40 @@
 #include <stdio.h>
+#include "esp_task_wdt.h"
 #include "led.h"
 #include "delay.h"
 #include "touch.h"
-#include "board.h"
 
 void app_main(void)
 {
+    esp_task_wdt_deinit(); // Desactiva el watchdog de tareas
     touch_init();
     while (1)
     {
-        set_led(255, 0, 0);
-        delay_s(1);
-        set_led(0, 255, 0);
-        delay_s(1);
-        set_led(0, 0, 255);
-        delay_s(1);
-        set_led(255, 0, 0);
-        delay_ms(200);
-        set_led(0, 255, 0);
-        delay_ms(200);
-        set_led(0, 0, 255);
-        delay_ms(200);
+        touch_update();
+        if (touch_pressed(TOUCH_PHOTO))
+        {
+            set_led(255, 0, 0);
+        }
+        if (touch_pressed(TOUCH_PLAY_PAUSE))
+        {
+            set_led(0, 255, 0);
+        }
+        if (touch_pressed(TOUCH_RECORD))
+        {
+            set_led(0, 0, 255);
+        }
+        if (touch_pressed(TOUCH_NETWORK))
+        {
+            set_led(0, 255, 255);
+        }
+        if (touch_pressed(TOUCH_VOLUME_UP))
+        {
+            set_led(255, 0, 255);
+        }
+        if (touch_pressed(TOUCH_RING))
+        {
+            set_led(255, 255, 255);
+        }
+        delay_ms(100);
     }
 }
