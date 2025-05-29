c82cca297fa535c8cb2dcde2085ffde758ee9bae
Joaquín Cuitiño <3134745+Vextil@users.noreply.github.com>
2025-05-29 19:41:52 -0300

    Reducción de brillo de LEDs

--- Lab_2/Parte_C/main/main.c
+++ Lab_2/Parte_C/main/main.c
@@ -19,27 +19,27 @@ void app_main(void)
         touch_update();
         if (touch_pressed(TOUCH_PHOTO))
         {
-            set_led(255, 0, 0);
+            set_led(20, 0, 0);
         }
         if (touch_pressed(TOUCH_PLAY_PAUSE))
         {
-            set_led(0, 255, 0);
+            set_led(0, 20, 0);
         }
         if (touch_pressed(TOUCH_RECORD))
         {
-            set_led(0, 0, 255);
+            set_led(0, 0, 20);
         }
         if (touch_pressed(TOUCH_NETWORK))
         {
-            set_led(0, 255, 255);
+            set_led(0, 20, 20);
         }
         if (touch_pressed(TOUCH_VOLUME_UP))
         {
-            set_led(255, 0, 255);
+            set_led(20, 0, 20);
         }
         if (touch_pressed(TOUCH_RING))
         {
-            set_led(255, 255, 255);
+            set_led(20, 20, 20);
         }
         delay_ms(100);
     }
