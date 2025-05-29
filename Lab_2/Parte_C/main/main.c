d9815d101b8f473b781d88f1b7ffcc20966eaa04
Joaquín Cuitiño <3134745+Vextil@users.noreply.github.com>
2025-05-29 01:59:55 -0300

    Web server

---
+++ Lab_2/Parte_C/main/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "esp_task_wdt.h"
+#include "led.h"
+#include "delay.h"
+#include "touch.h"
+#include "wifi.h"
+#include "http.h"
+
+void app_main(void)
+{
+    esp_task_wdt_deinit(); // Desactiva el watchdog de tareas
+
+    init_sta();
+    start_webserver();
+    touch_init();
+
+    while (1)
+    {
+        touch_update();
+        if (touch_pressed(TOUCH_PHOTO))
+        {
+            set_led(255, 0, 0);
+        }
+        if (touch_pressed(TOUCH_PLAY_PAUSE))
+        {
+            set_led(0, 255, 0);
+        }
+        if (touch_pressed(TOUCH_RECORD))
+        {
+            set_led(0, 0, 255);
+        }
+        if (touch_pressed(TOUCH_NETWORK))
+        {
+            set_led(0, 255, 255);
+        }
+        if (touch_pressed(TOUCH_VOLUME_UP))
+        {
+            set_led(255, 0, 255);
+        }
+        if (touch_pressed(TOUCH_RING))
+        {
+            set_led(255, 255, 255);
+        }
+        delay_ms(100);
+    }
+}
