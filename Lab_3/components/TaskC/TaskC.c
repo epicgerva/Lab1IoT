7a35624f0ef912c917b036882f34baae9ce13edb
Gerva <hcpeques@gmail.com>
2025-06-12 19:14:38 -0300

    Update TaskC.c

--- Lab_3/components/TaskC/TaskC.c
+++ Lab_3/components/TaskC/TaskC.c
@@ -3,62 +3,55 @@
 #include <stdlib.h>
 #include "TaskB.h"
 
-static SemaphoreHandle_t mutex;
-static QueueHandle_t queue;
-static volatile int *r_ptr, *g_ptr, *b_ptr;
-
+// Callback del temporizador: actualiza el color compartido
 static void timer_callback(TimerHandle_t xTimer)
 {
     color_cmd_t *cmd = (color_cmd_t *)pvTimerGetTimerID(xTimer);
-    if (cmd)
+    if (cmd != NULL)
     {
-        if (xSemaphoreTake(mutex, portMAX_DELAY))
+        if (xSemaphoreTake(xColorMutex, portMAX_DELAY))
         {
-            *r_ptr = cmd->r;
-            *g_ptr = cmd->g;
-            *b_ptr = cmd->b;
-            xSemaphoreGive(mutex);
+            color.r = cmd->r;
+            color.g = cmd->g;
+            color.b = cmd->b;
+            xSemaphoreGive(xColorMutex);
         }
         vPortFree(cmd);
     }
     xTimerDelete(xTimer, 0);
 }
 
-static void TaskC(void *pvParameters)
+// TASK C: recibe comandos de color y los aplica
+void TaskC(void *pvParameters)
 {
-    color_cmd_t cmd;
     ESP_LOGI("TASK C", "TASK C starting");
+    color_cmd_t cmd;
     while (1)
     {
-        if (xQueueReceive(queue, &cmd, portMAX_DELAY))
+        if (xQueueReceive(color_cmd_queue, &cmd, portMAX_DELAY))
         {
             if (cmd.delay_s == 0)
             {
-                if (xSemaphoreTake(mutex, portMAX_DELAY))
+                if (xSemaphoreTake(xColorMutex, portMAX_DELAY))
                 {
-                    *r_ptr = cmd.r;
-                    *g_ptr = cmd.g;
-                    *b_ptr = cmd.b;
-                    xSemaphoreGive(mutex);
+                    color.r = cmd.r;
+                    color.g = cmd.g;
+                    color.b = cmd.b;
+                    xSemaphoreGive(xColorMutex);
                 }
             }
             else
             {
                 color_cmd_t *cmd_copy = pvPortMalloc(sizeof(color_cmd_t));
                 *cmd_copy = cmd;
-                TimerHandle_t timer = xTimerCreate("ColorTimer", pdMS_TO_TICKS(cmd.delay_s * 1000), pdFALSE, cmd_copy, timer_callback);
+                TimerHandle_t timer = xTimerCreate("ColorTimer",
+                                                   pdMS_TO_TICKS(cmd.delay_s * 1000),
+                                                   pdFALSE,
+                                                   cmd_copy,
+                                                   timer_callback);
+
                 xTimerStart(timer, 0);
             }
         }
     }
-}
-
-void start_task_c(QueueHandle_t color_cmd_queue, SemaphoreHandle_t xColorMutex, volatile int *r, volatile int *g, volatile int *b)
-{
-    queue = color_cmd_queue;
-    mutex = xColorMutex;
-    r_ptr = r;
-    g_ptr = g;
-    b_ptr = b;
-    xTaskCreate(TaskC, "Task C", 2048, NULL, 1, NULL);
 }
