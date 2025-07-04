0bf8688904a2f2281ee2496d97d1e2c4fe4b3737
SebaBerardi <sebastianberardi2@gmail.com>
2025-07-03 21:21:40 -0300

    MQTT a REPRO
    Luego agrego las cosas al main

---
+++ Lab_Final/Lab_FinalREPRO/components/comunicate_mqtt/include/comunicate_mqtt.h
@@ -0,0 +1,17 @@
+#ifndef COMUNICATE_MQTT_H
+#define COMUNICATE_MQTT_H
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
+#include "mqtt_client.h"
+
+// Conecta al broker MQTT y suscribe al topic_evento con Last Will Message
+void connect_mqtt(const char *uri, int puerto, const char *topic_evento);
+
+// Configura la cola y el topic para almacenar comandos recibidos
+void almacenar_eventos(QueueHandle_t queue, const char *queue_topic);
+
+// Configura el buffer y el topic para enviar eventos cuando se reciba "Enviar"
+void enviar_eventos_buffe(int *buffer, int buffer_size, const char *buffer_topic);
+
+#endif // MQTT_MODULE_H
